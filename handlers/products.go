6efcf69c391e2de2311953d3fa000488c2ad7342
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-26 19:47:12 +0700

    refactor: implement UserOrders interface and add utility methods for Menu, Order, and Invoice

--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,13 @@ package handlers
 
 import "time"
 
+type UserOrders interface{
+    GetID() int
+    GetName() string
+    GetPrice() int
+    CalculateSubtotal(quantity int) int
+}
+
 
 type Menu struct {
 	ID int
@@ -9,9 +16,37 @@ type Menu struct {
     Price int
 }
 
+func (m Menu) GetID() int{
+    return m.ID
+}
+
+func (m Menu) GetName() string{
+    return m.Name
+}
+
+func (m Menu) GetPrice() int{
+    return m.Price
+}
+
+func (m Menu) CalculateSubtotal(quantity int) int{
+    return m.Price * quantity
+}
+
 type Order struct {
-    Item     Menu
+    Item     UserOrders
     Quantity int
+}
+
+func (o Order) GetSubtotal() int{
+    return o.Item.CalculateSubtotal(o.Quantity)
+}
+
+func (o Order) GetItemName() string{
+    return o.Item.GetName()
+}
+
+func (o Order) GetItemPrice() int{
+    return o.Item.GetPrice()
 }
 
 type Invoice struct {
@@ -19,6 +54,18 @@ type Invoice struct {
     Date          time.Time
     Orders        []Order
     Total         int
+}
+
+func (inv Invoice) CalculateTotal() int{
+    total := 0
+	for _, order := range inv.Orders {
+		total += order.GetSubtotal()
+	}
+	return total
+}
+
+func (inv Invoice) GetOrderCount() int {
+	return len(inv.Orders)
 }
 
 var Orders []Order
